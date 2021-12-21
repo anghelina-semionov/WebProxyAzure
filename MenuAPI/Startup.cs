8de6ebd3064ff0d45c498e83d1c06d63823164ea
Anghelina Semionov <anghelina.semionov@isa.utm.md>
2021-12-21 13:45:21 +0200

    Startup.cs changed

--- MenuAPI/Startup.cs
+++ MenuAPI/Startup.cs
@@ -1,19 +1,13 @@
-using Common.Models;
+using MenuAPI.Models;
 using MenuAPI.Repositories;
 using MenuAPI.Services;
 using MenuAPI.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MenuAPI
 {
